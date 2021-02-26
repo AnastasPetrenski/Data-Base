20612f1a1c723f2081f250c2d0b65cc3d5be3c49
AnastasPetrenski <hloro83@gmail.com>
2021-02-26 21:40:30 +0200

    EntityRelation

---
+++ Entity Framework Core/04 Entity Relations/FootballBettingSystem/P03_FootballBetting.Data/Configure.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public class Configure
+    {
+        internal static string ConnectionString = "Server=.;Database=FootballBettingSystem;Integrated Security=true;";
+    }
+}
