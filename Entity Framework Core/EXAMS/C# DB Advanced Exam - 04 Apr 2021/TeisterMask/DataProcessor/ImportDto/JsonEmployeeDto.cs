c649f5faa5447fdbf5f7aa7071e8ad1fa53cb7df
AnastasPetrenski <hloro83@gmail.com>
2021-04-04 14:13:12 +0300

    EXAM 2021

---
+++ Entity Framework Core/EXAMS/C# DB Advanced Exam - 04 Apr 2021/TeisterMask/DataProcessor/ImportDto/JsonEmployeeDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    class JsonEmployeeDto
+    {
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Za-z0-9]{3,40}$")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [RegularExpression(@"^[\d]{3}-[\d]{3}-[\d]{4}$")]
+        public string Phone { get; set; }
+
+        public ICollection<int> Tasks { get; set; }
+    }
+}
